26837f9ce212870dcd9a7da60766e4d6203b08e6
Kertu Mikk <kertumik@tlu.ee>
2018-03-09 13:34:28 +0200

    counter

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const TYPER = function () {
   this.word = null
   this.wordMinLength = 5
   this.guessedWords = 0
-
+  this.counter = 10
   this.init()
 }
 
@@ -51,14 +51,19 @@ TYPER.prototype = {
     xmlhttp.open('GET', './lemmad2013.txt', true)
     xmlhttp.send()
   },
-
   start: function () {
     this.generateWord()
     this.word.Draw()
 
     window.addEventListener('keypress', this.keyPressed.bind(this))
+    this.start = new Date().getTime()
+    window.setInterval(this.loop.bind(this), 1)
   },
-
+  loop: function () {
+    this.word.Draw()
+    const currentTime = new Date().getTime()
+    this.counter = currentTime - this.startTime
+  },
   generateWord: function () {
     const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 5)
     const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
@@ -98,6 +103,10 @@ Word.prototype = {
     this.ctx.textAlign = 'center'
     this.ctx.font = '140px Courier'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
+
+    this.ctx.textAlign = 'left'
+    this.ctx.font = '140px Courier'
+    this.ctx.fillText(this.counter, 100, 100)
   },
 
   removeFirstLetter: function () {
@@ -124,4 +133,3 @@ window.onload = function () {
   window.typer = typer
 }
 /* POINTS */
-
